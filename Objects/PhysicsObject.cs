ae086ff500f730b74a92f04614400b4032697fa5
ItsDaGrizz <alexhbear@gmail.com>
2025-09-05 20:36:23 +0100

    Initial Physics Impementation

---
+++ Objects/PhysicsObject.cs
@@ -0,0 +1,57 @@
+﻿using RenderingEngine.Rendering;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenderingEngine.Objects
+{
+    public class PhysicsObject : DynamicObject
+    {
+        public float mass = 10;
+        public float massInv;
+
+        public float restitution = 1f;
+        private float g = 9.81f;
+
+        public Vector3D<float> ForceAccum = Vector3D<float>.Zero;
+        public Vector3D<float> Acceleration = Vector3D<float>.Zero;
+        public Vector3D<float> Velocity = Vector3D<float>.Zero;
+
+        public PhysicsObject(Mesh mesh) :
+            base(mesh)
+        {
+            massInv = 1 / mass;
+            Acceleration.Y = -g;
+        }
+
+
+        public void TickPhysics(double deltaTime)
+        {
+            float dt = (float) deltaTime;
+
+            //Apply Gravity
+            ForceAccum.Y -= g * mass;
+
+            //TODO: Collision Checks, etc
+            if (Position.Y <= 0 && Velocity.Y < 0)
+            {
+                Velocity.Y *= (float) -restitution;
+            }
+            
+
+            //Apply Force to Velocity   F = MA
+            Velocity += ForceAccum * massInv * dt;
+
+            //Update Position Accordingly
+            Position += Velocity * dt;            
+            
+
+            //Clear Forces at the end
+            ForceAccum = Vector3D<float>.Zero;
+        }
+    }
+}
---
+++ PhysicsObjectsHandler.cs
@@ -0,0 +1,57 @@
+﻿using RenderingEngine.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenderingEngine
+{
+    public static class PhysicsObjectsHandler
+    {
+        private static int bufferSize = 15;
+        private static PhysicsObject?[] PhysObjBuffer = new PhysicsObject[bufferSize];
+
+        private static int pointer = 0;
+        public static int AddObj(PhysicsObject obj)
+        {
+            if (pointer >= bufferSize - 1) 
+            { 
+                Console.WriteLine("Physics Obj Buffer Full, Cant add new object!");  
+                return -1; 
+            }
+            
+            PhysObjBuffer[pointer] = obj;
+
+            pointer++;
+
+            return (pointer - 1);
+        }
+
+        public static void RemoveObj(int id)
+        {
+            PhysicsObject? obj = PhysObjBuffer[id];
+
+            if (obj != null)
+            {
+                PhysObjBuffer[id] = null;
+            }
+            else { Console.WriteLine($"Object id {id.ToString()} is already null!"); }
+        }
+
+
+        public static void TickObjs(double deltaTime)
+        {
+            for (int i = 0; i < pointer; i++)
+            {
+                PhysicsObject? obj = PhysObjBuffer[i];
+                if (obj != null)
+                {
+                    obj.TickPhysics(deltaTime);
+                }
+            }
+        }
+
+
+    }
+}
